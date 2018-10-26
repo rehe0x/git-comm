71d8b3c23b7225eee5826b876292dfd2ef756666
xiech <xiechanghong@everimaging.com>
2018-10-26 11:24:26 +0800

    Revert " 1111"
    This reverts commit faee90c

--- src/main/java/com/example/git/gitcomm/GitCommApplication.java
+++ src/main/java/com/example/git/gitcomm/GitCommApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitCommApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GitCommApplication.class, args);
+    public static void main(String[] args1) {
+        System.out.println("2223");
+        System.out.println("git test");
+        System.out.println("git test111");
+        if(false){
+
+        }
+        SpringApplication.run(GitCommApplication.class, args1);
     }
 }
