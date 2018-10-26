fa7d76aa38ccc6b6de55316a5e8f230222187e89
xiech <xiechanghong@everimaging.com>
2018-10-26 10:20:27 +0800

    初始提交

---
+++ src/main/java/com/example/git/gitcomm/GitCommApplication.java
@@ -0,0 +1,12 @@
+package com.example.git.gitcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCommApplication.class, args);
+    }
+}
